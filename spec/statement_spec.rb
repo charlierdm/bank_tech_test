02eca15259cf25b1bb412670fbe28450b385d453
charlierdm <charlie.davismiller@gmail.com>
2021-04-07 14:57:08 +0100

    statement class tests updated to represent behaviour

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -6,10 +6,11 @@ describe Statement do
     expect{ subject.store_transaction(credit = 100, debit = nil, balance = 1100) }.to change { subject.account_history.length }.by(1)
   end
 
-  it 'stores the correct date of the users transcation' do
-    statement = Statement.new
-    statement.store_transaction(credit = 100, debit = nil, balance = 1100)
-    expect(statement.account_history[0][:date]).to eq(Time.now.strftime("%d/%m/%Y"))
+  it 'stores multiple, individual transactions' do
+    subject.store_transaction(credit = 100, debit = nil, balance = 1100)
+    subject.store_transaction(credit = 200, debit = nil, balance = 1300)
+    expect(subject.account_history.length).to be(2)
+    expect(subject.account_history[0][:balance]).not_to eq(subject.account_history[1][:balance])
   end
   
 end
