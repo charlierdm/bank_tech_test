595c40d863e1354040ac8bf1f7867a325b77d81c
charlierdm <charlie.davismiller@gmail.com>
2021-04-07 10:33:07 +0100

    account initializes with user specified balance

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,4 +2,9 @@ require 'account'
 
 describe Account do
   
+  it 'allows the user to set an initial balance' do
+    account = Account.new(1000)
+    expect(account.balance).to eq(1000)
+  end
+
 end
