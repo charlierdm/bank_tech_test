0d68ade564afc196de3cc7582432a6d4d24ff584
charlierdm <charlie.davismiller@gmail.com>
2021-04-07 21:00:41 +0100

    2 decimal places added for each value and tests updated

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -41,7 +41,7 @@ describe Account do
       allow(transaction).to receive(:deposit) { 50 }
       allow(statement).to receive(:store_transaction)
       allow(statement).to receive(:return_statement) {
-                            "date || credit || debit || balance\n07/04/2021 || 100 ||  || 1100\n07/04/2021 ||  || 500 || 600"
+                            "date || credit || debit || balance\n07/04/2021 || 100.00 ||  || 1100.00\n07/04/2021 ||  || 500.00 || 600.00"
                           }
     end
 
@@ -52,7 +52,7 @@ describe Account do
     it 'displays the transaction history to the user' do
       account.deposit(100)
       account.withdraw(500)
-      expect { account.return_statement }.to output("date || credit || debit || balance\n07/04/2021 || 100 ||  || 1100\n07/04/2021 ||  || 500 || 600\n").to_stdout
+      expect { account.view_statement }.to output("date || credit || debit || balance\n07/04/2021 || 100.00 ||  || 1100.00\n07/04/2021 ||  || 500.00 || 600.00\n").to_stdout
     end
   end
 end
